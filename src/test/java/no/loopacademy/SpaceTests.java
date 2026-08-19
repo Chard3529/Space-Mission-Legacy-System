8fc493f18ce699e93c5439cd1657db3980b0f18b
= <=>
2026-08-19 13:37:57 +0200

    test: added test for connecting to telemetry at adress

--- src/test/java/no/loopacademy/SpaceTests.java
+++ src/test/java/no/loopacademy/SpaceTests.java
@@ -1,46 +1,35 @@
-package no.loopacademy.tests;
-
-import no.loopacademy.TelemetryClient;
-import no.loopacademy.TelemetryReading;
+package no.loopacademy;
 
 
-import java.util.ArrayList;
-import java.util.List;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions.*;
+
 
 public class SpaceTests {
 
     @Test
-    public void check_If_type_Is_pressure_Or_temp(){
+    public void connectsToConfiguredTelemetryEndpointWhenCreated(){
+        String expectedResult = "Connecting to telemetry at " + Config.telemetryEndpoint;
 
-        String temp = "TEMP";
-        String pressure = "PRESSURE";
+        PrintStream originalOutput = System.out;
+        var output = new ByteArrayOutputStream();
 
-        TelemetryClient telemetryClient = new TelemetryClient();
-        TelemetryReading reading= telemetryClient.read();
+        try {
+            System.setOut(new PrintStream(output));
 
-        String actualType = reading.type;
+            new TelemetryClient();
 
-        assert actualType == temp || actualType == pressure;
-    }
+            String actualResult = output.toString().trim();
 
-    @Test
-    public void check_If_type_Is_pressure_Or_temp_bool(){
-        List<String> excpectedTypeList = new ArrayList<>();
+            assertEquals(expectedResult, actualResult);
+        }
+        finally {
+            System.setOut(originalOutput);
+        }
 
-        String temp = "TEMP";
-        String pressure = "PRESSURE";
-        excpectedTypeList.add(temp);
-        excpectedTypeList.add(pressure);
-        boolean expected_result = true;
-
-        TelemetryClient telemetryClient = new TelemetryClient();
-        TelemetryReading reading= telemetryClient.read();
-        boolean actual_result = excpectedTypeList.contains(reading.type);
-
-
-        assert actual_result == expected_result;
     }
 }

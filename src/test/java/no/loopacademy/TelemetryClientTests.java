2aa966bd3a06628d01364362e10af76ce8d5e5e7
= <=>
2026-08-19 14:23:27 +0200

    test: readTelemetryReading med feilende test for time millis

---
+++ src/test/java/no/loopacademy/TelemetryClientTests.java
@@ -0,0 +1,64 @@
+package no.loopacademy;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+
+public class TelemetryClientTests {
+
+    @Test
+    public void connectsToConfiguredTelemetryEndpointWhenCreated(){
+        String expectedResult = "Connecting to telemetry at " + Config.telemetryEndpoint;
+
+        PrintStream originalOutput = System.out;
+        var output = new ByteArrayOutputStream();
+
+        try {
+            System.setOut(new PrintStream(output));
+
+            new TelemetryClient();
+
+            String actualResult = output.toString().trim();
+
+            assertEquals(expectedResult, actualResult);
+        }
+        finally {
+            System.setOut(originalOutput);
+        }
+    }
+
+    @Test
+    public void readTelemetryReadingTypeIsTempOrPressure(){
+        boolean expectedResult = true;
+
+        String temp = "TEMP";
+        String pressure = "PRESSURE";
+
+        var telemetryClient = new TelemetryClient();
+
+        TelemetryReading telemetryReading = telemetryClient.read();
+
+        boolean actualResult = (telemetryReading.type.equals(temp) || telemetryReading.type.equals(pressure));
+
+       assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void readTelemetryReadingTimeStampIsWithinOneSecondOfCurrentTime(){
+        long currentTime = System.currentTimeMillis();
+        long expectedResult = currentTime + 1000;
+
+        var telemetryClient = new TelemetryClient();
+
+        TelemetryReading telemetryReading = telemetryClient.read();
+
+        long actualResult = telemetryReading.timestamp;
+
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+}
